f82013fdc90d33162dc745f3538636afde638e56
Andrew Wooldridge <triptych@gmail.com>
2022-12-12 03:56:40 +0000

    getting panels to work

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // created by Andrew Wooldridge triptych@gmail.com
 
 import { reactive, html, watch } from 'https://cdn.skypack.dev/@arrow-js/core';
+import gameData from './data/data.json' assert {type: 'json'};
+console.log(gameData);
 
 const data = reactive({
   tileWidth: 32, // pixels
@@ -12,6 +14,7 @@ const data = reactive({
   currTileY: 0, // current Y coord
   img: null, // image
   ctx: null, // canvas context
+  currPanel:"tiles" // current panel
 })
 
 
@@ -23,14 +26,54 @@ const renderTile = () => {
     data.ctx.drawImage(data.img, data.currTileX * data.tileWidth, data.currTileY * data.tileHeight, data.tileWidth, data.tileHeight, 0, 0, data.tileWidth, data.tileHeight);
   }
 }
+
 const bindEvents = () => {
   const inputX = document.getElementById('inputX');
   const inputY = document.getElementById('inputY');
+  const panels = document.querySelectorAll('.pnl-chooser');
   inputX.addEventListener('change', (e) => { data.currTileX = e.target.value });
   inputY.addEventListener('change', (e) => { data.currTileY = e.target.value });
+  panels.forEach((pnl) => {
+    console.log(pnl)
+    pnl.addEventListener('click', (e) => {
+      console.log('click', e.target.id);
+      data.currPanel = e.target.getAttribute('data-link');
+      
+    });
+  
+  });
+}
+
+const renderPanels = () => {
+  console.log('renderPanels', data.currPanel);
+  const panels = document.querySelectorAll('.pnl');
+  console.log(panels);
+  // if(panels.length < 1) return;
+  // for (let i = 0; i < panels.length; i++) {
+  //   panels[i].style.display = "none"
+  // }
+  panels.forEach((pnl) => {
+    pnl.style.display = "none";
+  })
+  switch (data.currPanel) {
+      case 'tiles':
+      document.querySelector('.pnl.tiles').style.display = "block";
+        break;
+      case 'data':
+        document.querySelector('.pnl.data').style.display = "block";
+        break;
+      case 'tools':
+      document.querySelector('.pnl.tools').style.display = "block";
+        break;
+      default:
+        console.log('renderPanels', data.currPanel)
+        break;
+  
+  }
 }
 
 watch(renderTile);
+watch(renderPanels);
 
 window.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("cnv_preview");
